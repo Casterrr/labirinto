98597133376c521dcd8c149aa184b594c5798dcb
Lucas Matheus Vieira Lúcio <lucmvl2003@gmail.com>
2023-08-20 13:24:07 -0300

    primeiro commit

---
+++ eh_possivel_sair.py
@@ -0,0 +1,4 @@
+from gera_labirinto import gera_lab, print_lab #importa as funções do módulo gera_lab
+
+lab = gera_lab(7,15) #gera um labirinto com 7 linhas e 15 colunas
+print_lab(lab) #imprime o lab
